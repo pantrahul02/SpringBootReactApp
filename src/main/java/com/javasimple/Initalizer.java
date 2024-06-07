fb29868db2330df835cd2d3229c729c3d728d928
pantrahul02 <pantrahul02@gmail.com>
2024-06-07 18:17:07 +0530

    initial springboot with react app commit

---
+++ src/main/java/com/javasimple/Initalizer.java
@@ -0,0 +1,38 @@
+package com.javasimple;
+
+import com.javasimple.model.Event;
+import com.javasimple.model.Group;
+import com.javasimple.repo.GroupRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import java.time.Instant;
+import java.util.Collections;
+import java.util.stream.Stream;
+
+@Component
+public class Initalizer implements CommandLineRunner {
+    private final GroupRepository repository;
+
+    public Initalizer(GroupRepository repository) {
+        this.repository = repository;
+    }
+
+    @Override
+    public void run(String... strings) {
+        Stream.of("Seattle JUG", "Denver JUG", "Dublin JUG",
+                "London JUG").forEach(name ->
+                repository.save(new Group(name))
+        );
+
+        Group djug = repository.findByName("Seattle JUG");
+        Event e = Event.builder().title("Micro Frontends for Java Developers")
+                .description("JHipster now has microfrontend support!")
+                .date(Instant.parse("2022-09-13T17:00:00.000Z"))
+                .build();
+        djug.setEvents(Collections.singleton(e));
+        repository.save(djug);
+
+        repository.findAll().forEach(System.out::println);
+    }
+}
